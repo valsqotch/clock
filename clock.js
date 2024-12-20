9303a1f98eedb56eb57f8b195b36052108d21b5c
valsqotch <136604293+valsqotch@users.noreply.github.com>
2024-12-21 04:52:48 +0800

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,7 +1,10 @@
 const main = document.getElementById("main");
 let mode=0; //0 for default, 1 for timer
-const ut = setInterval(updateTime,1000);
-const uc = setInterval(updateColor,100);
+let secondsCount=0; //used in the timer mode
+var ut = setInterval(updateTime,1000);
+var uc = setInterval(updateColor,100);
+var tm = setInterval(countTime,1000);
+clearInterval(tm);
 function updateTime(){
     let t = new Date;
     let str = ""
@@ -32,12 +35,38 @@ function updateColor(){
     }
     console.log(variable);
 }
-document.addEventListener("click",);
+document.addEventListener("click",alterMode);
+
 function alterMode(){
     if(!mode){ //go to timer mode
-        clearInterval
+        clearInterval(ut);
+        clearInterval(uc);
+        main.innerText="00:00";
+        secondsCount=0;
+        tm = setInterval(countTime,1000);
+        mode=1;
+        document.body.style.backgroundColor = "black";
     }
     else{//back to clock mode
-
+        clearInterval(tm);
+        updateColor();
+        updateTime();
+        ut = setInterval(updateTime,1000);
+        uc = setInterval(updateColor,100);
+        mode=0;
     }
+}
+function countTime(){
+    secondsCount++;
+    let str="";
+    if(secondsCount/60<=9){
+        str+="0";
+    }
+    str+=(secondsCount/60).toFixed(0)+":";
+    if(secondsCount%60<=9){
+        str+="0";
+    }
+    str+=(secondsCount%60).toFixed(0);
+    console.log(secondsCount);
+    main.innerText=str;
 }
